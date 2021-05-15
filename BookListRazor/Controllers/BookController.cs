eb501c22c367092ba1e1084e33e6758988888860
sandeep mittapally <sandeep.mittapally@hotmail.com>
2021-05-15 16:41:02 -0400

    Updated data tables

---
+++ BookListRazor/Controllers/BookController.cs
@@ -0,0 +1,43 @@
+﻿using BookListRazor.Model;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BookListRazor.Controllers
+{
+    [Route("api/Book")]
+    [ApiController]
+    public class BookController : Controller
+    {
+        private readonly ApplicationDbContext _db;
+
+        public BookController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetAll()
+        {
+            return Json(new { data = await _db.Book.ToListAsync() });
+        }
+
+        [HttpDelete]
+        public async Task<IActionResult> Delete(int id)
+        {
+            var bookfromDB = await _db.Book.FirstOrDefaultAsync(u => u.Id == id);
+            if(bookfromDB == null)
+            {
+                return Json(new { success = false, message = "Error while Deleting" });
+            }
+
+            _db.Book.Remove(bookfromDB);
+            await _db.SaveChangesAsync();
+            return Json(new { success = true, message = "Delete Successful" });
+
+        }
+    }
+}
