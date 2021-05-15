43ed0f236cfe4b073478d87016bb2ab778ed3f0a
sandeep mittapally <sandeep.mittapally@hotmail.com>
2021-05-15 14:22:17 -0400

    Added New migrations for Saving Book List New Razor Page for shwoing Book List with Edit, Delete Functionality New Create Book Razor Page

--- BookListRazor/Program.cs
+++ BookListRazor/Program.cs
@@ -21,6 +21,6 @@ namespace BookListRazor
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
                     webBuilder.UseStartup<Startup>();
-                });
+                }); 
     }
 }
